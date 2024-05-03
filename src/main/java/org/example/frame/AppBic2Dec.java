4247198340550dea3625184905137babdc1a06b6
Griga2 <griga.k.r.21@gmail.com>
2024-05-03 19:41:04 +0300

    Add application desktop

---
+++ src/main/java/org/example/frame/AppBic2Dec.java
@@ -0,0 +1,12 @@
+package org.example.frame;
+
+import javax.swing.*;
+
+public class AppBic2Dec extends JFrame {
+    public JFrame createApp(){
+
+        this.setSize(500,500);
+        this.setVisible(true);
+        return  this;
+    }
+}
