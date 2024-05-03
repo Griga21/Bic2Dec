95946c3360177557d597834f4dc8f9570f8dfe07
Griga2 <griga.k.r.21@gmail.com>
2024-05-03 19:41:15 +0300

    Add application class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,15 @@
 package org.example;
 
+import org.example.frame.AppBic2Dec;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int a = Bic2Dec(scanner.next());
+        AppBic2Dec appBic2Dec =new AppBic2Dec();
+        appBic2Dec.createApp();
     }
 
     public static int Bic2Dec(String number){
