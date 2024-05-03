b5fa3db53c648e5fb50456bae658fd06a391bd29
Griga2 <griga.k.r.21@gmail.com>
2024-05-03 20:00:59 +0300

    Add Labels

--- src/main/java/org/example/frame/AppBic2Dec.java
+++ src/main/java/org/example/frame/AppBic2Dec.java
@@ -1,12 +1,27 @@
 package org.example.frame;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class AppBic2Dec extends JFrame {
-    public JFrame createApp(){
+    private JPanel jPanel= new JPanel(new BorderLayout());
+    private JLabel title = new JLabel("Перевод из двоичной системы из счисления в десятичную");
+    private JLabel bic = new JLabel("Двоичная система");
+    private JLabel dec = new JLabel("Десятичная система");
 
-        this.setSize(500,500);
-        this.setVisible(true);
-        return  this;
+    public void createApp(){
+        setIconImage(new ImageIcon("src/main/resources/images.png").getImage());
+        setTitle("From Bic to Dec");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(500,500);
+        setVisible(true);
+        setLocationRelativeTo(null);
+        addFields();
+        add(jPanel);
+    }
+    public void addFields(){
+        jPanel.add(title, BorderLayout.NORTH);
+        jPanel.add(bic, BorderLayout.CENTER);
+        jPanel.add(dec, BorderLayout.CENTER);
     }
 }
