39f4d182c2e48ee1dabd5626883decad9c62e360
Griga2 <griga.k.r.21@gmail.com>
2024-05-03 20:01:20 +0300

    Add JFrame element

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,13 +2,16 @@ package org.example;
 
 import org.example.frame.AppBic2Dec;
 
+import javax.swing.*;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int a = Bic2Dec(scanner.next());
-        AppBic2Dec appBic2Dec =new AppBic2Dec();
+        //Test for terminal
+        // Scanner scanner = new Scanner(System.in);
+        //int a = Bic2Dec(scanner.next());
+
+        AppBic2Dec appBic2Dec = new AppBic2Dec();
         appBic2Dec.createApp();
     }
 
