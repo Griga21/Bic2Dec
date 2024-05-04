cbfc97011a467dd275ce51b830168c9aaf258ab3
Griga2 <griga.k.r.21@gmail.com>
2024-05-04 18:23:58 +0300

    Add check for correct input

--- src/main/java/org/example/frame/AppBic2Dec.java
+++ src/main/java/org/example/frame/AppBic2Dec.java
@@ -4,11 +4,13 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.example.Main.Bic2Dec;
 
 public class AppBic2Dec extends JFrame {
-    private JPanel controlPanel= new JPanel();
+    private JPanel controlPanel = new JPanel();
     private JLabel bic = new JLabel("Двоичная система");
     private JLabel dec = new JLabel("Десятичная система");
     private JTextField textFieldBic = new JTextField();
@@ -16,123 +18,125 @@ public class AppBic2Dec extends JFrame {
     private JButton translate = new JButton("Перевести");
     private JButton change = new JButton("Поменять");
     private boolean isBic;
+    private Pattern pattern;
+    private Matcher matcher;
 
-    public void createApp(){
+    public AppBic2Dec(){
+        pattern= Pattern.compile("[01]{8}");
+
+    }
+
+    public void createApp() {
         setIconImage(new ImageIcon("src/main/resources/images.png").getImage());
         setTitle("From Bic to Dec");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(450,200);
+        setSize(450, 200);
         setVisible(true);
         setLocationRelativeTo(null);
-        addFieldsDec();
-        addAction();
+        addAction(this);
+        prepareGUI();
     }
-    public void addFieldsDec(){
+
+    public void prepareGUI() {
+        controlPanel.removeAll();
+        controlPanel.repaint();
         GridBagLayout layout = new GridBagLayout();
         controlPanel.setLayout(layout);
         GridBagConstraints gbc = new GridBagConstraints();
 
+        if (!isBic)
+            addFieldsDec(gbc);
+        else
+            addFieldsBic(gbc);
+
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.insets = new Insets(10, 30, 0, 10);
+        controlPanel.add(translate, gbc);
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.gridx = 1;
+        gbc.gridy = 2;
+        gbc.insets = new Insets(10, 10, 0, 30);
+        controlPanel.add(change, gbc);
+        add(controlPanel);
+    }
+
+    public void addFieldsDec(GridBagConstraints gbc) {
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.gridx = 0;
         gbc.gridy = 0;
-        gbc.insets =new Insets(10, 30, 0, 0);
-        controlPanel.add(bic,gbc);
+        gbc.insets = new Insets(10, 30, 0, 0);
+        controlPanel.add(bic, gbc);
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.ipady = 10;
         gbc.ipadx = 100;
         gbc.gridx = 1;
         gbc.gridy = 0;
-        gbc.insets =new Insets(10, 0, 0, 30);
-        controlPanel.add(textFieldBic,gbc);
+        gbc.insets = new Insets(10, 0, 0, 30);
+        controlPanel.add(textFieldBic, gbc);
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.gridx = 0;
         gbc.gridy = 1;
-        gbc.insets =new Insets(10, 30, 0, 0);
-        controlPanel.add(dec,gbc);
+        gbc.insets = new Insets(10, 30, 0, 0);
+        controlPanel.add(dec, gbc);
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.gridx = 1;
         gbc.gridy = 1;
-        gbc.insets =new Insets(10, 0, 0, 30);
-        controlPanel.add(textFieldDec,gbc);
-
-        gbc.fill = GridBagConstraints.BOTH;
-        gbc.gridx = 0;
-        gbc.gridy = 2;
-        gbc.insets =new Insets(10, 30, 0, 10);
-        controlPanel.add(translate,gbc);
-
-        gbc.fill = GridBagConstraints.BOTH;
-        gbc.gridx = 1;
-        gbc.gridy = 2;
-        gbc.insets =new Insets(10, 10, 0, 30);
-        controlPanel.add(change,gbc);
-        this.add(controlPanel);
+        gbc.insets = new Insets(10, 0, 0, 30);
+        controlPanel.add(textFieldDec, gbc);
     }
-    public void addFieldsBic(){
 
-        GridBagLayout layout = new GridBagLayout();
-        controlPanel.setLayout(layout);
-        GridBagConstraints gbc = new GridBagConstraints();
+    public void addFieldsBic(GridBagConstraints gbc) {
 
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.gridx = 0;
         gbc.gridy = 0;
-        gbc.insets =new Insets(10, 30, 0, 0);
-        controlPanel.add(dec,gbc);
+        gbc.insets = new Insets(10, 30, 0, 0);
+        controlPanel.add(dec, gbc);
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.ipady = 10;
         gbc.ipadx = 100;
         gbc.gridx = 1;
         gbc.gridy = 0;
-        gbc.insets =new Insets(10, 0, 0, 30);
-        controlPanel.add(textFieldDec,gbc);
+        gbc.insets = new Insets(10, 0, 0, 30);
+        controlPanel.add(textFieldDec, gbc);
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.gridx = 0;
         gbc.gridy = 1;
-        gbc.insets =new Insets(10, 30, 0, 0);
-        controlPanel.add(bic,gbc);
+        gbc.insets = new Insets(10, 30, 0, 0);
+        controlPanel.add(bic, gbc);
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.gridx = 1;
         gbc.gridy = 1;
-        gbc.insets =new Insets(10, 0, 0, 30);
-        controlPanel.add(textFieldBic,gbc);
+        gbc.insets = new Insets(10, 0, 0, 30);
+        controlPanel.add(textFieldBic, gbc);
 
-        gbc.fill = GridBagConstraints.BOTH;
-        gbc.gridx = 0;
-        gbc.gridy = 2;
-        gbc.insets =new Insets(10, 30, 0, 10);
-        controlPanel.add(translate,gbc);
 
-        gbc.fill = GridBagConstraints.BOTH;
-        gbc.gridx = 1;
-        gbc.gridy = 2;
-        gbc.insets =new Insets(10, 10, 0, 30);
-        controlPanel.add(change,gbc);
-
-        this.add(controlPanel);
     }
-    private void addAction(){
+
+    private void addAction(JFrame frame) {
         translate.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if(!isBic)
+                if (!isBic) {
+                    matcher = pattern.matcher(textFieldBic.getText());
+                    if (matcher.find())
                     textFieldDec.setText(Integer.toString((Bic2Dec(textFieldBic.getText()))));
-                else
+                }
+                else {
                     textFieldBic.setText(Integer.toString((Bic2Dec(textFieldDec.getText()))));
+                }
             }
         });
         change.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 controlPanel.removeAll();
+                controlPanel.revalidate();
+                prepareGUI();
+                frame.setContentPane(controlPanel);
                 controlPanel.repaint();
-                if (isBic){
-                    addFieldsDec();
-                    isBic= true;
-                }else {
-                    addFieldsBic();
-                    isBic= false;
-                }
             }
         });
     }
